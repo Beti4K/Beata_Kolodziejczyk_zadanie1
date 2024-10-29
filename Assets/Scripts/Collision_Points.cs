6e6aecac8622964700d327f1763e83eef9000138
Beti4K <beata.kolodziejczyk2004@gmail.com>
2024-10-29 20:28:07 +0100

    Points Display
    Points are displayed both in Debug Log and on screen.
    
    Punkty wyświetlają się w Debug Logu oraz na ekranie.

--- Assets/Scripts/Collision_Points.cs
+++ Assets/Scripts/Collision_Points.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
 
 public class Collision_Points : MonoBehaviour
 {
     public GameObject player;
+    public GameObject scoreText;
     private int score;
 
     private void OnCollisionEnter(Collision collision)
@@ -15,6 +17,7 @@ public class Collision_Points : MonoBehaviour
         score = player.GetComponent<Player_Movement>().points;
 
         Debug.Log("Current points: " + score);
+        scoreText.GetComponent<TextMeshProUGUI>().text = "Points: "+score;
 
         this.gameObject.GetComponent<Renderer>().material.color = new Color32(Random.Range(0, 256).ConvertTo<byte>(), Random.Range(0, 256).ConvertTo<byte>(), Random.Range(0, 256).ConvertTo<byte>(), 255);
     }
